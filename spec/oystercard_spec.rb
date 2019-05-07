741507eb5bdc8f0c38684be8ae788677b771c253
Sam <samkitchen94@gmail.com>
2019-05-07 15:19:21 +0100

    deduct test added and passing

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,14 +7,20 @@ describe Oystercard do
 
   describe '#top_up' do
     it 'tops up the card' do
-      initial_balance = subject.balance
-      expect(subject.top_up(10)).to eq(initial_balance + 10)
+      expect { subject.top_up(10) }.to change { subject.balance }.by +10
     end
 
     it "has a maximum capacity of 90" do
       message = "you have reached maximum top up capacity of #{Oystercard::MAXIMUM_CAPACITY}"
       subject.top_up(Oystercard::MAXIMUM_CAPACITY)
       expect { subject.top_up(1) }.to raise_error message
+    end
+  end
+
+  describe '#deduct' do
+    it "deducts money from the oystercard" do
+      new_card = subject.top_up(10)
+      expect { subject.deduct(5) }.to change { subject.balance }.by -5
     end
   end
 end
