578dd1b145291a0107456973b62f8020409664dc
Sam <samkitchen94@gmail.com>
2019-05-07 15:56:33 +0100

    minimum fare charge test added and passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,9 +2,11 @@ class Oystercard
   attr_reader :balance
 
   MAXIMUM_CAPACITY = 90
+  MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(money)
@@ -16,8 +18,21 @@ class Oystercard
     @balance -= money
   end
 
+  def touch_in
+    fail "you don't have enough funds for a single journey" if enough_money?
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
   private
   def full?
     @balance >= MAXIMUM_CAPACITY
+  end
+
+  def enough_money?
+    @balance < MINIMUM_FARE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,4 +23,24 @@ describe Oystercard do
       expect { subject.deduct(5) }.to change { subject.balance }.by -5
     end
   end
+
+  describe '#touch_in' do
+    it 'changes in_journey to true' do
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      expect(subject.touch_in).to eq(true)
+    end
+    it 'raises an error if the balance is less than 1' do
+      message = "you don't have enough funds for a single journey"
+      subject.top_up(0.5)
+      expect { subject.touch_in }.to raise_error message
+    end
+  end
+
+  describe '#touch_out' do
+    it 'changes in_journey to false' do
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      subject.touch_in
+      expect(subject.touch_out).to eq(false)
+    end
+  end
 end
