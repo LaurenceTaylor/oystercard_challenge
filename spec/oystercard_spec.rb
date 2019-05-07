497433bef0d2f6e87bb2fe9a965c688d738f2bdd
Sam <samkitchen94@gmail.com>
2019-05-07 14:45:27 +0100

    top up balance test added and passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it 'has a default balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  it 'tops up the card' do
+    initial_balance = subject.balance
+    expect(subject.top_up(10)).to eq(initial_balance + 10)
+  end
 end
