3c1e940c52be0aebe69ed31a63e07e01108e6509
Sam <samkitchen94@gmail.com>
2019-05-07 15:08:20 +0100

    test added to check maximum capacity of 90 and passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,19 @@
 class Oystercard
   attr_reader :balance
 
+  MAXIMUM_CAPACITY = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    fail "you have reached maximum top up capacity of #{MAXIMUM_CAPACITY}" if full?
     @balance += money
+  end
+
+  private
+  def full?
+    @balance >= MAXIMUM_CAPACITY
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,8 +5,16 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  it 'tops up the card' do
-    initial_balance = subject.balance
-    expect(subject.top_up(10)).to eq(initial_balance + 10)
+  describe '#top_up' do
+    it 'tops up the card' do
+      initial_balance = subject.balance
+      expect(subject.top_up(10)).to eq(initial_balance + 10)
+    end
+
+    it "has a maximum capacity of 90" do
+      message = "you have reached maximum top up capacity of #{Oystercard::MAXIMUM_CAPACITY}"
+      subject.top_up(Oystercard::MAXIMUM_CAPACITY)
+      expect { subject.top_up(1) }.to raise_error message
+    end
   end
 end
