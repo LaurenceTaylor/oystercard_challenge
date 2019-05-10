d446f7007eaa9212660b91faf52924d9639c332f
Laurence <ltaylorapp@gmail.com>
2019-05-10 15:01:26 +0100

    Added finish_journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,8 +2,13 @@ class Journey
 
   attr_reader :entry_station, :journey
 
-  def initialize(entry_station)
+  def initialize(entry_station = nil)
     @entry_station = entry_station
     @journey = {entry_station => nil}
+  end
+
+  def finish_journey(exit_station)
+    @journey[@entry_station] = exit_station
+    @entry_station = nil
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,13 +2,33 @@ require 'journey'
 
 describe Journey do
   let(:station_double) { double(:station, name: 'Liverpool Street', zone: 1) }
+  let(:exit_station_double) { double(:station, name: 'Bank', zone: 1) }
   subject { Journey.new(station_double) }
 
   it 'should store entry station' do
     expect(subject.entry_station).to eq(station_double)
   end
 
+  it 'should have entry_station be nil by default' do
+    journey = Journey.new
+    expect(journey.entry_station.nil?).to eq(true)
+  end
+
   it 'should store entry station in a hash' do
     expect(subject.journey).to eq({ station_double => nil })
+  end
+
+  describe '#finish_journey' do
+    before(:each) do
+      subject.finish_journey(exit_station_double)
+    end
+
+    it 'should add exit station to journey hash' do
+      expect(subject.journey).to eq({ station_double => exit_station_double })
+    end
+
+    it 'should change entry_station to nil' do
+      expect(subject.entry_station.nil?).to eq(true)
+    end
   end
 end
